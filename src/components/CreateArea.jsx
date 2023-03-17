5233277d53d7cf7781f48a7ecf3bff2f9415f818
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-03-17 16:45:26 +0600

    input handled

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,18 +2,36 @@ import React, {useState} from "react";
 
 function CreateArea() {
 
-  const [InputText, SetInputText] = useState("");
+  const [InputText, SetInputText] = useState({
+    title: "",
+    content: ""
+  });
 
-  function onInput(event){
-    var input=event.target.value;
-    SetInputText(input);
+  function handleData(event){
+    var name = event.target.name;
+    var value = event.target.value;
+
+    SetInputText(prevValue => {
+      if (name==="title"){
+        return {
+          title: value,
+          content: prevValue.content
+        }
+      }
+      else if (name==="content"){
+        return {
+          title: prevValue.title,
+          content: value
+        }
+      }
+    });
   }
 
   return (
     <div>
       <form>
-        <input onChange={onInput} name="title" placeholder="Title" value={InputText}/>
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input onChange={handleData} name="title" value={InputText.title} placeholder="Title"/>
+        <textarea onChange={handleData} name="content" value={InputText.content} placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
