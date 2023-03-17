399f1d3996627159759295fa061f9823da1d6d14
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-03-17 16:17:30 +0600

    Title state added

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea() {
+
+  const [InputText, SetInputText] = useState("");
+
+  function onInput(event){
+    var input=event.target.value;
+    SetInputText(input);
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
+        <input onChange={onInput} name="title" placeholder="Title" value={InputText}/>
         <textarea name="content" placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
