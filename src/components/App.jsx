54f0d396b6bf3602ada45a7f96dd144e9fa6e399
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-03-17 16:56:34 +0600

    addItem function created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [Notes, setNotes] = useState([]);
+
+  function addItems(InputText){
+    setNotes(prevValue=>{
+      return [...prevValue, InputText];
+    })
+  }
+
   return (
     <div>
       <Header />
